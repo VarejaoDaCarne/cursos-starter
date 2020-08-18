f78c80b9efef8e9f12176fe166c18761cb2e41ab
Gabriel Rios <60109015+grioos@users.noreply.github.com>
2020-08-18 12:17:33 -0300

    Navigation settings

--- reactjs/huntweb/src/App.js
+++ reactjs/huntweb/src/App.js
@@ -1,15 +1,15 @@
 import React from 'react';
+import Routes from './routes'
 
 import './styles.css'
 
 import Header from './components/Header'
-import Main from './pages/main'
 
 function App() {
   return (
     <div className="App">
         <Header />
-        <Main />
+        <Routes />
     </div>
   );
 }
