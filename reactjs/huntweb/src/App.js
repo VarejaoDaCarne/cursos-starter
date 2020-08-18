24f1a55a3644b0fd35a0f141eace9edfc0c9cbcc
Gabriel Rios <60109015+grioos@users.noreply.github.com>
2020-08-18 10:47:42 -0300

    creating header

--- reactjs/huntweb/src/App.js
+++ reactjs/huntweb/src/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
 
+import './styles.css'
+
+import Header from './components/Header'
+
 function App() {
   return (
     <div className="App">
-      <h1>Hello</h1>
+        <Header />
     </div>
   );
 }
