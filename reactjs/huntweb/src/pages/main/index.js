067f0b6843d63c3d1f4227c325191b3eeb36b736
Gabriel Rios <60109015+grioos@users.noreply.github.com>
2020-08-18 11:17:21 -0300

    Storing in stage

--- reactjs/huntweb/src/pages/main/index.js
+++ reactjs/huntweb/src/pages/main/index.js
@@ -2,6 +2,10 @@ import React, { Component } from 'react'
 import api from '../../services/api'
 
 export default class Main  extends Component {
+    state = {
+        products: []
+    }
+
     componentDidMount() {
         this.loadProducts()
     }
@@ -9,10 +13,16 @@ export default class Main  extends Component {
     loadProducts = async () => {
         const response = await api.get('/products')
 
-        console.log(response.data.docs)
+        this.setState({products: response.data.docs})
     }
 
     render () {
-        return <h1>Hello</h1>  
+        return (
+            <div className="product-list">
+                {this.state.products.map(product => (
+                    <h2 key={product._id}>{product.title}</h2>
+                ))}
+            </div>
+        )  
     }
 }
