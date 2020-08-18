4a0586c42f5da7f44c0438a86af5ccbef71cfabb
Gabriel Rios <60109015+grioos@users.noreply.github.com>
2020-08-18 11:07:34 -0300

    fetching products from api

--- reactjs/huntweb/src/App.js
+++ reactjs/huntweb/src/App.js
@@ -3,11 +3,13 @@ import React from 'react';
 import './styles.css'
 
 import Header from './components/Header'
+import Main from './pages/main'
 
 function App() {
   return (
     <div className="App">
         <Header />
+        <Main />
     </div>
   );
 }
