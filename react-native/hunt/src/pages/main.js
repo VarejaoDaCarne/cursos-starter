f900c394322d71dae1374f5be9a0baee8edbf275
Gabriel Rios <60109015+grioos@users.noreply.github.com>
2020-08-20 11:01:03 -0300

    Products list

--- react-native/hunt/src/pages/main.js
+++ react-native/hunt/src/pages/main.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 
 import { View, Text } from 'react-native'
 import api from '../services/api'
+import { FlatList, TouchableOpacity } from 'react-native-gesture-handler'
 
 export default class Main extends Component {
     state = {
@@ -20,13 +21,25 @@ export default class Main extends Component {
         this.setState({ docs })
     }
 
+    renderItem = ({ item }) => (
+        <View>
+            <Text>{item.title}</Text>
+            <Text>{item.description}</Text>
+
+            <TouchableOpacity onPress={() => {}}>
+                <Text>Acessar</Text>
+            </TouchableOpacity>
+        </View>
+    )
+    
     render() {
         return (
             <View>
-                <Text>Página Main</Text>
-                {this.state.docs.map(product => (
-                    <Text key={product._id}>{product.title}</Text>
-                ))}
+                <FlatList
+                    data={this.state.docs}
+                    keyExtractor={item => item._id}
+                    renderItem={this.renderItem}
+                />
             </View>
         )
     }
