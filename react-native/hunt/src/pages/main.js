4c1835a4264562c5dcb5d74dabbf483fa13c1400
Gabriel Rios <60109015+grioos@users.noreply.github.com>
2020-08-20 10:44:00 -0300

    Fetching products from api

--- react-native/hunt/src/pages/main.js
+++ react-native/hunt/src/pages/main.js
@@ -1,8 +1,21 @@
 import React, { Component } from 'react'
 
 import { View, Text } from 'react-native'
+import api from '../services/api'
 
 export default class Main extends Component {
+    componentDidMount() {
+        this.loadProducts()
+    }
+
+    loadProducts = async () => {
+        const response = await api.get('/products')
+
+        const { docs } = response.data
+
+        console.log(docs)
+    }
+
     render() {
         return (
             <View>
