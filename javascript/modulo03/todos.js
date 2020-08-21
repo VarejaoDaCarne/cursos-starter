7a34bdc82571b70b7655557bd85c2dd7fb992c1d
Gabriel Rios <60109015+grioos@users.noreply.github.com>
2020-08-21 08:28:56 -0300

    Starting app

---
+++ javascript/modulo03/todos.js
@@ -0,0 +1,9 @@
+const listElement = document.querySelector('#app ul')
+const inputElement = document.querySelector('#app input')
+const buttonElement = document.querySelector('#app button')
+
+const todos = [
+    'Fazer café',
+    'Estudar Javascript',
+    'Acessar comunidade da Rocketseat'
+]
